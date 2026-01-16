b602208f58ee5681b7a60fbc99f6f5cc2d3af1b5
MarKisses <igor3003moroz@gmail.com>
2026-01-16 15:51:52 +0200

    Adds /info command and refactors info menu handling
    Introduces a dedicated command to display bot info, allowing users to access the about menu directly from chat. Refactors info menu logic for improved code reuse and maintainability by leveraging a shared service menu utility, leading to cleaner exception handling and consistent UI.

--- app/bot/handlers/main_menu.py
+++ app/bot/handlers/main_menu.py
@@ -3,8 +3,9 @@ from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from bot.filters import FromUserRequired
-from bot.keyboards.main_menu import main_menu_keyboard
+from bot.keyboards.main_menu import main_menu_keyboard, back_to_main_menu_keyboard
 from bot.utils import replace_service_menu, show_service_menu
+from bot.menus.about import text
 from logger import create_logger
 
 logger = create_logger(__name__)
@@ -21,4 +22,17 @@ async def start(message: Message, state: FSMContext):
         chat_id=message.chat.id,
         text="Головне меню бота:",
         reply_markup=main_menu_keyboard(),
+    )
+    
+@router.message(Command("info"), FromUserRequired())
+async def info(message: Message, state: FSMContext):
+    await state.clear()
+    logger.info(f"User {message.from_user.id} requested info command.")
+
+    await show_service_menu(
+        bot=message.bot,
+        chat_id=message.chat.id,
+        text=text,
+        reply_markup=back_to_main_menu_keyboard(),
+        old_msg_id=message.message_id,
     )
