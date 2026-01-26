8e6bdad5cf7ff1f347fa11a45626dc0af6d1e017
MarKisses <igor3003moroz@gmail.com>
2026-01-26 14:16:14 +0200

    Improves Redis client creation with connection pooling

--- app/storage/__init__.py
+++ app/storage/__init__.py
@@ -1,24 +1,28 @@
 from aiogram.fsm.storage.redis import RedisStorage
-from redis.asyncio import Redis
-from .user_storage import UserStorage
-from .subscription_storage import SubscriptionStorage
-
 from config import settings
+from redis.asyncio import ConnectionPool, Redis
+
+from .subscription_storage import SubscriptionStorage
+from .user_storage import UserStorage
 
 
 def create_redis_client() -> Redis:
-    return Redis(
+    connection_pool = ConnectionPool(
         host=settings.redis.host,
         port=settings.redis.port,
         db=settings.redis.db,
         password=settings.redis.password,
         username=settings.redis.username,
         decode_responses=True,
+        max_connections=25,
     )
+
+    return Redis(connection_pool=connection_pool)
 
 
 def create_storage(redis: Redis):
     return RedisStorage(redis=redis)
+
 
 _redis = create_redis_client()
 
@@ -27,4 +31,10 @@ user_storage = UserStorage(_redis)
 subscription_storage = SubscriptionStorage(_redis)
 
 
-__all__ = ["create_redis_client", "create_storage", "user_storage", "subscription_storage", "fsm_storage"]
+__all__ = [
+    "create_redis_client",
+    "create_storage",
+    "user_storage",
+    "subscription_storage",
+    "fsm_storage",
+]
